17affcb29c17772858189216e47d9bbc88972e68
DESKTOP-IG540Q0\jo626 <jo62698476@gmail.com>
2023-01-09 02:31:41 +0900

    [add] ch07

---
+++ ch07_챗봇 학습 툴 만들기/ch07_1.py
@@ -0,0 +1,22 @@
+import pymysql
+
+db = None
+try:
+    # DB 호스트 정보에 맞게 입력해주세요
+    db = pymysql.connect(
+        host='127.0.0.1',
+        user='root',
+        passwd='whwnsgh99*',
+        db='chatbot',
+        charset='utf8'
+    )
+    print("DB 연결 성공")
+
+
+except Exception as e:
+    print(e)
+
+finally:
+    if db is not None:
+        db.close()
+        print("DB 연결 닫기 성공")
